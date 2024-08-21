26d8a6e82571f0431a05c60f13c06633b31dc3ab
Avinash kumar sajjan <133846619+avi-610@users.noreply.github.com>
2024-08-21 12:16:56 +0530

    Delete main.rs

--- main.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-    println!("Hello, world!");
-
-}
-// This line is known as 'comment line' which will not be executed with the program.
-//we have created a main function under which 'hello ,world!' will be printed with the help of println!()
-//println!() adds a new line character (enter) at the end, so the second text is printed in the next line. 
-//main() function is the entry point of every rust program. We have to call every function inside main().
